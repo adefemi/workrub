486c8423f5e58818320dad9b724684ceb7e7cd8b
Adefemi <adefemi.oseni@algorimsng.com>
2020-01-18 14:39:59 +0100

    updated

--- components/date-controller.php
+++ components/date-controller.php
@@ -1,9 +1,9 @@
-<link href='/demo/assets/scripts/packages/core/main.css' rel='stylesheet' />
-<link href='/demo/assets/scripts/packages/daygrid/main.css' rel='stylesheet' />
+<link href='<?php echo $GLOBALS['base_url'].'/assets/scripts/packages/core/main.css'?>' rel='stylesheet' />
+<link href='<?php echo $GLOBALS['base_url'].'/assets/scripts/packages/daygrid/main.css' ?>' rel='stylesheet' />
 
-<script src='/demo/assets/scripts/packages/core/main.js'></script>
-<script src='/demo/assets/scripts/packages/daygrid/main.js'></script>
-<script src='/demo/assets/scripts/packages/interaction/main.js'></script>
+<script src='<?php echo $GLOBALS['base_url'].'/assets/scripts/packages/core/main.js'?>'></script>
+<script src='<?php echo $GLOBALS['base_url'].'/assets/scripts/packages/daygrid/main.js' ?>'></script>
+<script src='<?php echo $GLOBALS['base_url'].'/assets/scripts/packages/interaction/main.js'?>'></script>
 
 <div class="zone-label">
     Time Zone: GMT+1
